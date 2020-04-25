320e0114f5ae91f671ecb5ae10a19f5b26758728
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-04-25 07:30:37 +0300

    created a dynamic route and news.html

--- app/views.py
+++ app/views.py
@@ -11,3 +11,11 @@ def index():
 
     message = 'Hello World'
     return render_template('index.html',message = message)
+
+@app.route('/news/<news_id>')
+def news(news_id):
+
+    '''
+    View news page function that returns the news details page and its data
+    '''
+    return render_template('news.html',id = news_id)
