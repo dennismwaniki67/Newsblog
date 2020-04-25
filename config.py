fe47253a983b72cd3596e43b7e9a551292446437
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-04-25 11:00:13 +0300

    updated our config.py file.

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
+import os
 class Config:
     '''
     General configuration parent class
     '''
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&apiKey={}'
     NEWS_ARTICLE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     
 
 
@@ -27,3 +30,8 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}  
